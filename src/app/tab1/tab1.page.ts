0fdeae315ea0bafb3227fb93686c6b5ec3d5908b
Diego <diegoandrestrom04@gmail.com>
2021-08-24 08:29:06 -0500

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { analyzeFileForInjectables } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,6 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+
+  posts = [
+    {
+      imgUser: 'https://randomuser.me/api/portraits/men/27.jpg',
+      username: 'rodrigues_0215',
+      liked: true,
+      saved: false,
+      location: 'Riga Latvia',
+      imgs: ['https://fondosmil.com/fondo/17010.jpg']
+    },
+    {
+      imgUser: 'https://upload.wikimedia.org/wikipedia/commons/1/14/Animal_diversity.png',
+      username: 'animal_good',
+      liked: false,
+      saved: true,
+      location: null,
+      imgs: ['https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2018/08/fondos-pantalla-full-hd-animales_4.jpg']
+    },
+    {
+      imgUser: 'https://randomuser.me/api/portraits/women/69.jpg',
+      username: 'sofia_1484',
+      liked: false,
+      saved: true,
+      location: 'Riga Latvia',
+      imgs: ['https://fondosmil.com/fondo/17010.jpg']
+    },
+    {
+      imgUser: 'https://randomuser.me/api/portraits/lego/1.jpg',
+      username: 'lego_oficial',
+      liked: true,
+      saved: false,
+      location: null,
+      imgs: ['https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2018/08/fondos-pantalla-full-hd-animales_4.jpg']
+    },
+  ];
+
+
+  opts = {
+    slidesPerView: 4.8,
+    spaceBetween: 11,
+    slidesOffsetBefore: 0
+  };
 
   constructor() {}
 
